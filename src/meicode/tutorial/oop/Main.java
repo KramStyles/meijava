37df63e7855f9ce4d338982891c3c08eddbfb7d7
Michael Jamie <kramstyles@outlook.com>
2021-11-06 15:45:15 +0100

    working with getters, setters and constructors

--- src/meicode/tutorial/oop/Main.java
+++ src/meicode/tutorial/oop/Main.java
@@ -1,7 +1,15 @@
 package meicode.tutorial.oop;
 
 public class Main {
-    Phone iPhone = new Phone(); // Instantiating our class
+    public static void main(String[] args) {
+        Phone iPhone = new Phone("Iphone 7 Pro");
+        Phone android = new Phone("Phantom 6", 800, 4, 4);
 
+        iPhone.setName("iPhone 6s");
+        android.setName("Camon 12 Air");
+
+        System.out.println(iPhone.getName());
+        iPhone.playMusic("21 Pilots Heathens");
+    }
 
 }
--- src/meicode/tutorial/oop/Phone.java
+++ src/meicode/tutorial/oop/Phone.java
@@ -1,8 +1,31 @@
 package meicode.tutorial.oop;
 
 public class Phone {
-    String name;
+    private String name;
     int screenSize;
     int ram;
     int camera;
+
+    public Phone(String name, int screenSize, int ram, int camera) {
+        this.name = name;
+        this.screenSize = screenSize;
+        this.ram = ram;
+        this.camera = camera;
+    }
+
+    public Phone(String name) {
+        this.name = name;
+    }
+
+    public void playMusic(String track){
+        System.out.println("Playing song: " + track);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
