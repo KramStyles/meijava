5f03b38d762f1d75c24bd07e49d359045fbb6eda
Michael Jamie <kramstyles@outlook.com>
2021-11-07 22:37:16 +0100

    working with extends keyword

---
+++ src/meicode/tutorial/oop/Bird.java
@@ -0,0 +1,7 @@
+package meicode.tutorial.oop;
+
+public class Bird extends Dog {
+    public Bird(String name, String color, int legs, boolean hasTail) {
+        super(name, color, legs, hasTail);
+    }
+}
--- src/meicode/tutorial/oop/Main.java
+++ src/meicode/tutorial/oop/Main.java
@@ -2,6 +2,10 @@ package meicode.tutorial.oop;
 
 public class Main {
     public static void main(String[] args) {
+        animal();
+    }
+
+    static void phone(){
         Phone iPhone = new Phone("Iphone 7 Pro");
         Phone android = new Phone("Phantom 6", 800, 4, 4);
 
@@ -10,6 +14,11 @@ public class Main {
 
         System.out.println(iPhone.getName());
         iPhone.playMusic("21 Pilots Heathens");
+    }
+
+    static void animal(){
+        Bird phoenix = new Bird("Pipi", "Purple & Grey", 3, true);
+        System.out.println(phoenix.getName());
     }
 
 }
