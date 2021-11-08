b24375e9bfbcf035112cd22ddea5a6615d44ecb3
Michael Jamie <kramstyles@outlook.com>
2021-11-08 23:01:09 +0100

    working with interfaces 2

--- src/meicode/tutorial/interfaces/App.java
+++ src/meicode/tutorial/interfaces/App.java
@@ -1,4 +1,9 @@
 package meicode.tutorial.interfaces;
 
 public class App {
+    public static void main(String[] args) {
+        Car carInterface = new ElectricCar("Honda");
+        carInterface.move(200);
+        carInterface.start();
+    }
 }
--- src/meicode/tutorial/interfaces/ElectricCar.java
+++ src/meicode/tutorial/interfaces/ElectricCar.java
@@ -1,14 +1,27 @@
 package meicode.tutorial.interfaces;
 
 public class ElectricCar implements Car{
+    private String name;
+
+    public ElectricCar(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public void start() {
-
+        System.out.println("Electric car is started");
     }
 
     @Override
     public void move(int Speed) {
-
+        System.out.println(this.getName() + " is moving at " + Speed + "mph");
     }
 }
