5ac372467a7236cb17a0ea70c05dbf2aebdae800
Michael Jamie <kramstyles@outlook.com>
2021-11-09 15:09:14 +0100

    working with the dog class

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+    public int eyes;
+    public String name;
+    public String skinColor;
+    public int legs;
+
+    public void eating(){
+        System.out.println("I am eating");
+    }
+
+    public void print(){
+        System.out.println("I am hungry");
+    }
+}
--- src/second.java
+++ src/second.java
@@ -1,6 +1,11 @@
 public class second{
     public static void main(String[] args) {
-        System.out.println(Math.max(2,5));
-        System.out.println((int)Math.round(Math.random() * 100));
+        Dog bingo = new Dog();
+        Dog mario = new Dog();
+        bingo.eating();
+
+        mario.print();
     }
+
+
 }
